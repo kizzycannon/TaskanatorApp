9c9eaec5f065acd4e0907fb08b9891ee06c4e4c7
luken684 <luken684@gmail.com>
2021-03-21 15:53:33 +0000

    Added Data To List View And Border To List

--- app/src/main/java/com/example/taskanatorapp/MainActivity.java
+++ app/src/main/java/com/example/taskanatorapp/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.taskanatorapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /* Main Actvity */
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> testData = new ArrayList<>();
+    ArrayAdapter<String> testAdapter;
+    ListView listView;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testData.add("HELLO");
+        testData.add("WORLD");
+        testAdapter = new ArrayAdapter<String>(this, R.layout.activity_list, R.id.listText, testData);
+        listView = findViewById(R.id.MyTasks);
+        listView.setAdapter(testAdapter);
+        listView.setBackground(this.getDrawable(R.drawable.list_view_border));
     }
 }
