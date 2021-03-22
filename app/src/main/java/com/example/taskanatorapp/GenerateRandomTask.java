a56fd5123d6262956a29e5cbd5841011a7a53bf2
Samantha Cannon <samantha.cannon.2020@uni.strath.ac.uk>
2021-03-22 16:04:21 +0000

    started work on random generate

--- app/src/main/java/com/example/taskanatorapp/GenerateRandomTask.java
+++ app/src/main/java/com/example/taskanatorapp/GenerateRandomTask.java
@@ -5,8 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+
+import java.util.Random;
+import java.util.ArrayList;
 
 public class GenerateRandomTask extends AppCompatActivity {
+
+    //ArrayList of tasks to be chosen from which meet the criteria
+    private ArrayList<Task> tasksMeetingCriteriaList;
+    //duration specified as maximum time for random task
+    private int duration;
+    private String category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +26,21 @@ public class GenerateRandomTask extends AppCompatActivity {
         setContentView(R.layout.activity_generate_random_task);
     }
 
+    public void onProgressChanged (SeekBar seekBar, int progressValue, boolean fromUser) {}
+
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+    public void updateFromSeekBar(View view) {
+        EditText editDuration = (EditText) findViewById(R.id.editTextNumberGRT);
+        SeekBar seekBarProgress = (SeekBar) findViewById(R.id.seekBarGRT);
+        editDuration.setText(seekBarProgress.getProgress());
+    }
+
     public void buttonGenerateRandomTask(View view) {
         Intent intent = new Intent(this, YourRandomTask.class);
+        EditText editDuration = (EditText) findViewById(R.id.editTextNumberGRT);
         startActivity(intent);
     }
 }
