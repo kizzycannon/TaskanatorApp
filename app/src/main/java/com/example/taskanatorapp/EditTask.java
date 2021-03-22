e78a2e144b7f51f33cdb9da07eeac72d69797f33
luken684 <luken684@gmail.com>
2021-03-22 13:26:13 +0000

    Sort Strings.xml

---
+++ app/src/main/java/com/example/taskanatorapp/EditTask.java
@@ -0,0 +1,14 @@
+package com.example.taskanatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_task);
+    }
+}
