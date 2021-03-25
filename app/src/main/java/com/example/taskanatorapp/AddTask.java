59af5f96bb28ae2d965975b894bd011c1e60daa3
Samantha Cannon <samantha.cannon.2020@uni.strath.ac.uk>
2021-03-25 14:31:23 +0000

    -added null checks to add task page -added limitation of length 2 number for duration inputs

--- app/src/main/java/com/example/taskanatorapp/AddTask.java
+++ app/src/main/java/com/example/taskanatorapp/AddTask.java
@@ -28,31 +28,45 @@ public class AddTask extends AppCompatActivity {
         buttonAddTask.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                String taskName = null;
-                String taskCategory = null;
-                String taskDescription = null;
-                int taskDuration = 0;
+
                 ArrayList<Task> activeTaskList = system.getActiveTasks();
 
                 TextView taskCategoryTitle = (TextView)findViewById(R.id.addTaskNameCategory);
                 TextView taskNameTitle = (TextView)findViewById(R.id.addTaskNameTitle);
+                TextView taskDescriptionTitle = (TextView) findViewById(R.id.addTaskDescriptionTitle);
+                TextView taskDurationTitle = (TextView) findViewById(R.id.addTaskDurationTitle);
                 EditText taskNameView = (EditText) findViewById(R.id.addNewTaskName);
                 Spinner taskCategoryView = (Spinner)findViewById(R.id.addTaskCategory);
                 EditText taskDescriptionView = (EditText) findViewById(R.id.addNewTaskDescription);
                 EditText taskDurationView = (EditText) findViewById(R.id.addNewTaskDuration);
 
-                if (taskNameView.getText() != null && taskDescriptionView.getText() != null && taskDescriptionView.getText() != null
-                && taskDurationView.getText() != null) {
-                    taskName = taskNameView.getText().toString();
-                    taskCategory = taskCategoryView.getSelectedItem().toString();
-                    taskDescription = taskDescriptionView.getText().toString();
-                    taskDuration = Integer.parseInt(taskDurationView.getText().toString());
-                }
-                if (taskName == null || taskCategory == null || taskDescription == null || taskDuration == 0) {
-                    taskNameTitle.setText(R.string.add_edit_tasks_task_error_null);
+                // error check for null inputs
+                if (taskNameView.getText() == null || taskNameView.getText().length() == 0) {
+                    taskNameTitle.setText(R.string.add_edit_tasks_task_name_empty_error);
                     taskCategoryTitle.setText(R.string.add_edit_tasks_task_category);
+                    taskDescriptionTitle.setText(R.string.add_edit_tasks_task_description);
+                    taskDurationTitle.setText(R.string.add_edit_tasks_task_duration);
                     return;
                 }
+                if (taskDescriptionView.getText() == null || taskDescriptionView.getText().length() == 0) {
+                    taskNameTitle.setText(R.string.add_edit_tasks_task_name);
+                    taskCategoryTitle.setText(R.string.add_edit_tasks_task_category);
+                    taskDescriptionTitle.setText(R.string.add_edit_tasks_task_description_empty_error);
+                    taskDurationTitle.setText(R.string.add_edit_tasks_task_duration);
+                    return;
+                }
+                if (taskDurationView.getText() == null || taskDurationView.getText().length() == 0) {
+                    taskNameTitle.setText(R.string.add_edit_tasks_task_name);
+                    taskCategoryTitle.setText(R.string.add_edit_tasks_task_category);
+                    taskDescriptionTitle.setText(R.string.add_edit_tasks_task_description);
+                    taskDurationTitle.setText(R.string.add_edit_tasks_task_duration_empty_error);
+                    return;
+                }
+
+                String taskName = taskNameView.getText().toString();
+                String taskCategory = taskCategoryView.getSelectedItem().toString();
+                String taskDescription = taskDescriptionView.getText().toString();
+                int taskDuration = Integer.parseInt(taskDurationView.getText().toString());
 
                 ArrayList<Task> taskList = system.getAllTasks();
                 for (Task task: taskList) {
@@ -67,24 +81,6 @@ public class AddTask extends AppCompatActivity {
                     taskCategoryTitle.setText(R.string.add_edit_tasks_task_category_error);
                     return;
                 }
-                if(taskNameView.getText().length() == 0){
-
-                    taskNameTitle.setText(R.string.add_edit_tasks_task_name_empty_error);
-                    taskCategoryTitle.setText(R.string.add_edit_tasks_task_category);
-                    return;
-                }
-                if(taskDescriptionView.getText().length() == 0){
-
-                    taskNameTitle.setText(R.string.add_edit_tasks_task_description_empty_error);
-                    taskCategoryTitle.setText(R.string.add_edit_tasks_task_category);
-                    return;
-                }
-                if(taskDurationView.getText().length() == 0){
-
-                    taskNameTitle.setText(R.string.add_edit_tasks_task_duration_empty_error);
-                    taskCategoryTitle.setText(R.string.add_edit_tasks_task_category);
-                    return;
-                }
 
                 system.createNewTask(taskName, taskCategory, taskDescription, taskDuration);
 
@@ -92,14 +88,10 @@ public class AddTask extends AppCompatActivity {
                 Boolean switchStatus = activeSwitch.isChecked();
 
                 if(switchStatus){
-                    //system.getActiveTasks().add(new Task(taskName, taskCategory, taskDescription, taskDuration));
                     int indexOfNew = (system.getAllTasks().size())-1;
                     system.addToActiveTasks(system.getAllTasks().get(indexOfNew));
-
                 }
-
                 PrefConfig.saveSystem(AddTask.this, system);
-
                 Intent intent = new Intent(AddTask.this, ManageTasks.class);
                 startActivity(intent);
             }
